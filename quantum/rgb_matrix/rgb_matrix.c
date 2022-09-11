ac93f79f63d23fe104dbb73cb32d21c4f6cbd802
MichaelDavidGK <39009683+MichaelDavidGK@users.noreply.github.com>
2022-09-11 01:00:26 +0100

    updated

--- quantum/rgb_matrix/rgb_matrix.c
+++ quantum/rgb_matrix/rgb_matrix.c
@@ -86,12 +86,16 @@ __attribute__((weak)) RGB rgb_matrix_hsv_to_rgb(HSV hsv) {
 #endif
 
 #if !defined(RGB_MATRIX_STARTUP_MODE)
-#    ifdef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#        define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
+#    ifdef OPENRGB_ENABLE
+#        define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_OPENRGB_DIRECT
 #    else
+#        ifndef DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#            define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
+#        else
 // fallback to solid colors if RGB_MATRIX_CYCLE_LEFT_RIGHT is disabled in userspace
-#        define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
-#    endif
+#           define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+#       endif
+#   endif
 #endif
 
 #if !defined(RGB_MATRIX_STARTUP_HUE)
