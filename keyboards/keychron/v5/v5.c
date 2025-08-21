ae81cf300bc3619d1ca3741639db26409cf44654
Tom Sadler <t.d.sadler88@gmail.com>
2025-08-21 03:10:34 +0100

    added v5 ansi knob

--- keyboards/keychron/v5/v5.c
+++ keyboards/keychron/v5/v5.c
@@ -67,8 +67,8 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) { return false; }
+void rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    // if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) { return false; }
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
 
     if (host_keyboard_led_state().caps_lock) {
@@ -78,7 +78,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
         }
     }
-    return true;
+    return;
 }
 
 #endif // CAPS_LOCK_LED_INDEX
